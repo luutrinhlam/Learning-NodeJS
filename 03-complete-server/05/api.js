0d620ec733ceee15d140607106717d20d7951ff7
Lam <luutrinhlam@gmail.com>
2022-01-27 03:09:07 +0700

    Ultil page 130

--- 03-complete-server/05/api.js
+++ 03-complete-server/05/api.js
@@ -2,35 +2,70 @@ const Products = require('./products')
 
 module.exports = {
     getProduct,
-    listProducts
+    listProducts,
+    createProduct,
+    deleteProduct,
+    editProduct
 }
 
+// async function getProduct(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     const { id } = req.params
+//     try {
+//         const product = await Products.get(id)
+//         if (!product) return next()
+//         res.json(product)
+//     } catch (err) {
+//         res.status(500).json({ error: err.message })
+//     }
+// }
 async function getProduct(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
     const { id } = req.params
-    try {
-        const product = await Products.get(id)
-        if (!product) return next()
-        res.json(product)
-    } catch (err) {
-        res.status(500).json({ error: err.message })
-    }
+    const product = await Products.get(id)
+    if (!product) return next()
+    res.json(product)
 }
 
 
-async function listProducts(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    const { offset = 0, limit = 25, tag } = req.query
-    try {
-        res.json(await Products.list(
-            {
-                offset: Number(offset),
-                limit: Number(limit),
-                tag
-            }
-        ))
+// async function listProducts(req, res) {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     const { offset = 0, limit = 25, tag } = req.query
+//     try {
+//         res.json(await Products.list(
+//             {
+//                 offset: Number(offset),
+//                 limit: Number(limit),
+//                 tag
+//             }
+//         ))
 
-    } catch (err) {
-        res.status(500).json({ error: err.message })
-    }
+//     } catch (err) {
+//         res.status(500).json({ error: err.message })
+//     }
+// }
+async function listProducts(req, res) {
+    const { offset = 0, limit = 25, tag } = req.query
+    const products = await Products.list({
+        offset: Number(offset),
+        limit: Number(limit),
+        tag
+    })
+    res.json(products)
+}
+
+
+async function createProduct(req, res, next) {
+    console.log('request body:', req.body)
+    res.json(req.body)
+}
+
+
+async function deleteProduct(req, res, next) {
+    res.json({ success: true })
+}
+
+
+async function editProduct(req, res, next) {
+    // console.log(req.body)
+    res.json(req.body)
 }
--- 03-complete-server/05/server.js
+++ 03-complete-server/05/server.js
@@ -1,13 +1,22 @@
 const express = require('express')
 
 const api = require('./api')
+const middleware = require('./middleware')
 
 const port = process.env.PORT || 1337
 
 const app = express()
 
+app.use(middleware.cors)
 app.get('/products', api.listProducts)
 app.get('/products/:id', api.getProduct)
+app.post('/products', api.createProduct)
+app.put('/products/:id', api.editProduct)
+app.delete('/products/:id', api.deleteProduct)
+app.use(middleware.handleError)
+app.use(middleware.notFound)
+
+
 
 const server = app.listen(port, () =>
     console.log(`Server listening on port ${port}`)
